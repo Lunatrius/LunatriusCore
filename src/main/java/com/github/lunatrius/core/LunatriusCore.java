413dc7eb3c70a60a39f0353d6eef7ab4cdbe38a9
Lunatrius <jadran.kotnik@gmail.com>
2014-11-25 12:05:14 +0100

    Made the mod optional on both sides.

--- src/main/java/com/github/lunatrius/core/LunatriusCore.java
+++ src/main/java/com/github/lunatrius/core/LunatriusCore.java
@@ -10,11 +10,20 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkCheckHandler;
+import cpw.mods.fml.relauncher.Side;
+
+import java.util.Map;
 
 @Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY)
 public class LunatriusCore {
     @SidedProxy(serverSide = Reference.PROXY_SERVER, clientSide = Reference.PROXY_CLIENT)
     public static CommonProxy proxy;
+
+    @NetworkCheckHandler
+    public boolean checkModList(Map<String, String> versions, Side side) {
+        return true;
+    }
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
