5d4d4d0a09947d6e8bd738c3aed4cdf6e812906d
Lunatrius <jadran.kotnik@gmail.com>
2015-05-25 19:22:49 +0200

    Added helper methods that take int instead of BlockPos.

--- src/main/java/com/github/lunatrius/core/util/BlockPosHelper.java
+++ src/main/java/com/github/lunatrius/core/util/BlockPosHelper.java
@@ -7,8 +7,12 @@ import java.util.Iterator;
 
 public class BlockPosHelper {
     public static Iterable<MBlockPos> getAllInBox(final BlockPos from, final BlockPos to) {
-        final BlockPos posMin = new BlockPos(Math.min(from.getX(), to.getX()), Math.min(from.getY(), to.getY()), Math.min(from.getZ(), to.getZ()));
-        final BlockPos posMax = new BlockPos(Math.max(from.getX(), to.getX()), Math.max(from.getY(), to.getY()), Math.max(from.getZ(), to.getZ()));
+        return getAllInBox(from.getX(), from.getY(), from.getZ(), to.getX(), to.getY(), to.getZ());
+    }
+
+    public static Iterable<MBlockPos> getAllInBox(final int fromX, final int fromY, final int fromZ, final int toX, final int toY, final int toZ) {
+        final BlockPos posMin = new BlockPos(Math.min(fromX, toX), Math.min(fromY, toY), Math.min(fromZ, toZ));
+        final BlockPos posMax = new BlockPos(Math.max(fromX, toX), Math.max(fromY, toY), Math.max(fromZ, toZ));
         return new Iterable<MBlockPos>() {
             @Override
             public Iterator<MBlockPos> iterator() {
@@ -54,8 +58,12 @@ public class BlockPosHelper {
     }
 
     public static Iterable<MBlockPos> getAllInBoxXZY(final BlockPos from, final BlockPos to) {
-        final BlockPos posMin = new BlockPos(Math.min(from.getX(), to.getX()), Math.min(from.getY(), to.getY()), Math.min(from.getZ(), to.getZ()));
-        final BlockPos posMax = new BlockPos(Math.max(from.getX(), to.getX()), Math.max(from.getY(), to.getY()), Math.max(from.getZ(), to.getZ()));
+        return getAllInBoxXZY(from.getX(), from.getY(), from.getZ(), to.getX(), to.getY(), to.getZ());
+    }
+
+    public static Iterable<MBlockPos> getAllInBoxXZY(final int fromX, final int fromY, final int fromZ, final int toX, final int toY, final int toZ) {
+        final BlockPos posMin = new BlockPos(Math.min(fromX, toX), Math.min(fromY, toY), Math.min(fromZ, toZ));
+        final BlockPos posMax = new BlockPos(Math.max(fromX, toX), Math.max(fromY, toY), Math.max(fromZ, toZ));
         return new Iterable<MBlockPos>() {
             @Override
             public Iterator<MBlockPos> iterator() {
@@ -101,8 +109,12 @@ public class BlockPosHelper {
     }
 
     public static Iterable<MBlockPos> getAllInBoxYXZ(final BlockPos from, final BlockPos to) {
-        final BlockPos posMin = new BlockPos(Math.min(from.getX(), to.getX()), Math.min(from.getY(), to.getY()), Math.min(from.getZ(), to.getZ()));
-        final BlockPos posMax = new BlockPos(Math.max(from.getX(), to.getX()), Math.max(from.getY(), to.getY()), Math.max(from.getZ(), to.getZ()));
+        return getAllInBoxYXZ(from.getX(), from.getY(), from.getZ(), to.getX(), to.getY(), to.getZ());
+    }
+
+    public static Iterable<MBlockPos> getAllInBoxYXZ(final int fromX, final int fromY, final int fromZ, final int toX, final int toY, final int toZ) {
+        final BlockPos posMin = new BlockPos(Math.min(fromX, toX), Math.min(fromY, toY), Math.min(fromZ, toZ));
+        final BlockPos posMax = new BlockPos(Math.max(fromX, toX), Math.max(fromY, toY), Math.max(fromZ, toZ));
         return new Iterable<MBlockPos>() {
             @Override
             public Iterator<MBlockPos> iterator() {
