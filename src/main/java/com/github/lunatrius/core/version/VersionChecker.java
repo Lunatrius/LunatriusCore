eb99b93d4fa2d7db83de14c57c70c4db1c5a60d6
Lunatrius <jadran.kotnik@gmail.com>
2015-10-10 18:26:50 +0200

    Replaced the default User-Agent.

--- src/main/java/com/github/lunatrius/core/version/VersionChecker.java
+++ src/main/java/com/github/lunatrius/core/version/VersionChecker.java
@@ -15,6 +15,7 @@ import net.minecraftforge.fml.common.versioning.DefaultArtifactVersion;
 
 import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -61,10 +62,12 @@ public class VersionChecker {
                         return;
                     }
 
-                    URL url = new URL(String.format(VER_CHECK_API_URL, Reference.MINECRAFT, VER_CHECK_API_VER));
-                    InputStream con = url.openStream();
-                    String data = new String(ByteStreams.toByteArray(con));
-                    con.close();
+                    final URL url = new URL(String.format(VER_CHECK_API_URL, Reference.MINECRAFT, VER_CHECK_API_VER));
+                    final URLConnection connection = url.openConnection();
+                    connection.addRequestProperty("User-Agent", Reference.MODID + "/" + Reference.VERSION);
+                    final InputStream inputStream = connection.getInputStream();
+                    final String data = new String(ByteStreams.toByteArray(inputStream));
+                    inputStream.close();
 
                     Map<String, Object> json = new Gson().fromJson(data, Map.class);
 
